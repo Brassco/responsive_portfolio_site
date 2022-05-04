da4cc54a0ebc0ae4af981e2de636d41a417eeaa7
Brassco <tonny.brassco@gmail.com>
2022-05-04 21:29:40 +0300

    completed Skills section

--- assets/js/main.js
+++ assets/js/main.js
@@ -27,3 +27,23 @@ function linkAction() {
 }
 
 navLink.forEach( n => n.addEventListener('click', linkAction))
+
+
+/*===== ACCORDION SKILLS =====*/
+const skillsHeader = document.querySelectorAll('.skills__header');
+const skillsContent = document.getElementsByClassName('skills__content');
+
+function toggleSkills() {
+   let itemClass = this.parentNode.className;
+   for(i = 0; i < skillsContent.length; i++) {
+       skillsContent[i].className = 'skills__content skills__close'
+   }
+
+   if (itemClass == 'skills__content skills__close') {
+       this.parentNode.className = 'skills__content skills__open'
+   }
+}
+
+skillsHeader.forEach( el => {
+    el.addEventListener('click', toggleSkills)
+})
